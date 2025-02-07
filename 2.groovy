0fb69e96317d35cf727e80b98240f7c0683b588c
evlc <dodgedodgedoggy@gmail.com>
2025-02-07 16:53:22 +0700

    2: limit at 80, better tests

--- 2.groovy
+++ 2.groovy
@@ -1,7 +1,7 @@
 class Karatsuba {
     static BigInteger multiply(BigInteger x, BigInteger y) {
         int n = Math.max(x.toString().length(), y.toString().length())
-        if (n <= 1) {
+        if (n <= 80) {
             return x * y
         }
 
@@ -20,13 +20,10 @@ class Karatsuba {
     }
 }
 
-assert Karatsuba.multiply(BigInteger.valueOf(3),          BigInteger.valueOf(5))         == 15
-assert Karatsuba.multiply(BigInteger.valueOf(12),         BigInteger.valueOf(12))        == 144
-assert Karatsuba.multiply(BigInteger.valueOf(123),        BigInteger.valueOf(456))       == 56088
-assert Karatsuba.multiply(BigInteger.valueOf(1000),       BigInteger.valueOf(1000))      == 1000000
-assert Karatsuba.multiply(BigInteger.valueOf(101),        BigInteger.valueOf(101))       == 10201
-assert Karatsuba.multiply(BigInteger.valueOf(12345),      BigInteger.valueOf(67890))     == 838102050
-assert Karatsuba.multiply(BigInteger.valueOf(99999),      BigInteger.valueOf(99999))     == 9999800001
-assert Karatsuba.multiply(BigInteger.valueOf(1234567890), BigInteger.valueOf(987654321)) ==  new BigInteger("1219326311126352690")
-assert Karatsuba.multiply(new BigInteger("12345678901234567890"), new BigInteger("98765432109876543210")) == new BigInteger("1219326311370217952237463801111263526900")
+assert Karatsuba.multiply(BigInteger.valueOf(3), BigInteger.valueOf(15)) == 45
+assert Karatsuba.multiply(BigInteger.valueOf(2), BigInteger.valueOf(12323)) == 24646
+assert Karatsuba.multiply(BigInteger.valueOf(123123), BigInteger.valueOf(3322233223322)) == 409043321155074606
+assert Karatsuba.multiply(BigInteger.valueOf(1000), BigInteger.valueOf(10001000)) == 10001000000
+assert Karatsuba.multiply(BigInteger.valueOf(101010101010101), BigInteger.valueOf(101010010101010110)) == 10203031323334353525151413121110
+assert Karatsuba.multiply(new BigInteger("1818181818181818181818181818181818181818"), new BigInteger("92929292929292929292929292929")) == new BigInteger("168962350780532598714416896234545454545437649219467401285583103764922")
 println("All tests passed!")
