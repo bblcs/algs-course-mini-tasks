5d50689a88c649e5f945c1ab5bf687c7ce5bb38a
evlc <dodgedodgedoggy@gmail.com>
2025-02-22 17:55:37 +0700

    6: nlogned

--- 6.rs
+++ 6.rs
@@ -1,43 +1,51 @@
 impl Solution {
     pub fn sort_array(mut nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len() - 1;
-        Self::merge_sort(&mut nums, 0, n);
+        let n = nums.len();
+        let mut buf = vec![0; n];
+        Self::merge_sort(&mut nums, &mut buf, 0, n);
         nums
     }
 
-    fn merge_sort(arr: &mut Vec<i32>, l: usize, r: usize) {
-        if l < r {
-            let m = l + (r - l) / 2;
-            Self::merge_sort(arr, l, m);
-            Self::merge_sort(arr, m + 1, r);
-            Self::merge(arr, l, m, r);
-        }
-    }
-
-    fn merge(arr: &mut Vec<i32>, mut start: usize, mut mid: usize, end: usize) {
-        let mut start2 = mid + 1;
-
-        if (arr[mid] <= arr[start2]) {
+    fn merge_sort(arr: &mut [i32], buf: &mut [i32], start: usize, end: usize) {
+        if end - start <= 1 {
             return;
         }
 
-        while start <= mid && start2 <= end {
-            if arr[start] <= arr[start2] {
-                start += 1;
+        let mid = start + (end - start) / 2;
+        Self::merge_sort(arr, buf, start, mid);
+        Self::merge_sort(arr, buf, mid, end);
+
+        Self::merge(arr, buf, start, mid, end);
+    }
+
+    fn merge(arr: &mut [i32], buf: &mut [i32], start: usize, mid: usize, end: usize) {
+        let mut i = start;
+        let mut j = mid;
+        let mut k = start;
+
+        while i < mid && j < end {
+            if arr[i] <= arr[j] {
+                buf[k] = arr[i];
+                i += 1;
             } else {
-                let val = arr[start2];
-                let mut idx = start2;
-
-                while idx != start {
-                    arr[idx] = arr[idx - 1];
-                    idx -= 1;
-                }
-
-                arr[start] = val;
-                start += 1;
-                mid += 1;
-                start2 += 1;
+                buf[k] = arr[j];
+                j += 1;
             }
+            k += 1;
         }
+
+        while i < mid {
+            buf[k] = arr[i];
+            i += 1;
+            k += 1;
+        }
+
+        while j < end {
+            buf[k] = arr[j];
+            j += 1;
+            k += 1;
+        }
+
+        arr[start..end].copy_from_slice(&buf[start..end]);
     }
 }
