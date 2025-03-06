1b849405e5a2fa101aa9c3130444703a81da660f
evlc <dodgedodgedoggy@gmail.com>
2025-03-06 16:02:05 +0700

    13: fixed

--- 13.cpp
+++ 13.cpp
@@ -1,17 +1,17 @@
 #include <algorithm>
 #include <vector>
 
-template <typename T> using Wells = std::vector<std::pair<T, T>>;
+template <typename T>
+auto median(const std::vector<T>& v) {
+    auto n = v.size();
+    std::vector<T> t = v;
 
-template <typename T> auto sol(const Wells<T> &wells) {
-  auto compare_by_y = [](const auto &a, const auto &b) {
-    return a.second < b.second;
-  };
-
-  auto min_y =
-      (*std::min_element(wells.begin(), wells.end(), compare_by_y)).second;
-  auto max_y =
-      (*std::max_element(wells.begin(), wells.end(), compare_by_y)).second;
-
-  return (min_y + max_y) / 2;
+    if (n % 2 == 0) {
+        std::nth_element(t.begin(), t.begin() + n / 2, t.end());
+        std::nth_element(t.begin(), t.begin() + (n - 1) / 2, t.end());
+        return (t[(n - 1) / 2] + t[n / 2]) / 2;
+    } else {
+        std::nth_element(t.begin(), t.begin() + n / 2, t.end());
+        return t[n / 2];
+    }
 }
